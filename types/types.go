3f5994b515c64f3fa81ee9fac81dec1fa4b02610
Fabio Kusaba <fabinhokb44@gmail.com>
2025-06-08 22:38:09 -0300

    add RegisterPayload

---
+++ types/types.go
@@ -0,0 +1,10 @@
+package types
+
+type UserStore interface {
+}
+
+type RegisterPayload struct {
+	Username string `json:"username" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
