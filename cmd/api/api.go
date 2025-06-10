722113111d28017621c719d2df574da899af3a4e
Fabio Kusaba <fabinhokb44@gmail.com>
2025-06-09 21:12:08 -0300

    add db to APIServer

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,19 +1,23 @@
 package api
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
+	"github.com/fabiokusaba/go-rest-api-tutorial/services/users"
 	"github.com/gorilla/mux"
 )
 
 type APIServer struct {
 	addr string
+	db   *sql.DB
 }
 
-func NewAPIServer(addr string) *APIServer {
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
 	return &APIServer{
 		addr: addr,
+		db:   db,
 	}
 }
 
@@ -22,6 +26,9 @@ func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 
 	// register services
+	userStore := users.NewUserStore(s.db)
+	userHandler := users.NewHandler(userStore)
+	userHandler.RegisterRoutes(router)
 
 	// cors configuration
 	// c := cors.New(cors.Options{
